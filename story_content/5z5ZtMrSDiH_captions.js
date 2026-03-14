166dd85e1890af52fb62654a5325e664eba04a9b
sainihimani106-bit <saini.himani106@gmail.com>
2026-03-14 23:16:50 +0530

    Add files via upload

---
+++ story_content/5z5ZtMrSDiH_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5z5ZtMrSDiH_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.110.36211.0%0D%0A%0D%0A00:00:00.100%20--%3E%2000:00:00.970%0D%0A[safety%20sound]%0D%0A%0D%0A"}]})
